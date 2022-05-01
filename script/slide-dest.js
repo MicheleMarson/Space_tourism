31a2cf07352f812b03ff1f89cc642919aee94071
Mihaela Marson <mitchmarson666@gmail.com>
2022-05-01 12:55:26 +0200

    fixes

--- script/slide-dest.js
+++ script/slide-dest.js
@@ -9,15 +9,17 @@ $(() => {
 	const img = $(".left img");
 	navElem.click(function (e) {
 		let id = +e.target.id;
-		$(".nav-content, .bottom-content,.left img").fadeOut(300, () => {
-			$(".des-nav").find(`li#${id}`).addClass("active");
-			$(`.des-nav li:not(#${id})`).removeClass("active");
-			img.attr("src", data.destinations[id].images.webp);
-			title.text(data.destinations[id].name);
-			text.text(data.destinations[id].description);
-			avgDist.text(data.destinations[id].distance);
-			travelTime.text(data.destinations[id].travel);
-			$(".nav-content, .bottom-content,.left img").fadeIn();
-		});
+		if (id === id) {
+			$(".nav-content, .bottom-content,.left img").fadeOut(300, () => {
+				$(".des-nav").find(`li#${id}`).addClass("active");
+				$(`.des-nav li:not(#${id})`).removeClass("active");
+				img.attr("src", data.destinations[id].images.webp);
+				title.text(data.destinations[id].name);
+				text.text(data.destinations[id].description);
+				avgDist.text(data.destinations[id].distance);
+				travelTime.text(data.destinations[id].travel);
+				$(".nav-content, .bottom-content,.left img").fadeIn();
+			});
+		}
 	});
 });
